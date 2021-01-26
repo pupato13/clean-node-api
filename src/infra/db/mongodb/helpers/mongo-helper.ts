015a9ca00e9a1a4602bfa61ffbe12b536ffa6ffb
Diego Pupato <pupato.diego@gmail.com>
2021-01-27 12:41:33 +1300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -9,11 +9,18 @@ export const MongoHelper = {
             useUnifiedTopology: true,
         });
     },
+
     async disconnect(): Promise<void> {
         await this.client.close();
     },
 
     getCollection(name: string): Collection {
         return this.client.db().collection(name);
+    },
+
+    map: (collection: any): any => {
+        const { _id, ...collectionWithoutId } = collection;
+
+        return Object.assign({}, collectionWithoutId, { id: _id });
     },
 };
