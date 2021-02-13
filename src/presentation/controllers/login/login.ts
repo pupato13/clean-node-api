618b0a3df8ba2d482e1f312f6890c7e95d4904a4
Diego Pupato <pupato.diego@gmail.com>
2021-02-14 10:33:10 +1300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -18,15 +18,15 @@ export class LoginController implements IController {
 
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
         try {
+            const requiredFields = ["email", "password"];
+
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(field));
+                }
+            }
+
             const { email, password } = httpRequest.body;
-
-            if (!email) {
-                return badRequest(new MissingParamError("email"));
-            }
-
-            if (!password) {
-                return badRequest(new MissingParamError("password"));
-            }
 
             const isValid = this.emailValidator.isValid(email);
 
