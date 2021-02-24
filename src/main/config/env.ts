e073ba636bb45b617f1ff6615f591b9b729a1df2
Diego Pupato <pupato.diego@gmail.com>
2021-02-25 12:54:06 +1300

    build: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,7 @@
 export default {
-    mongoUrl:
-        process.env.MONGO_URL || "mongodb://localhost:27017/clean-node-api",
+    // mongodb://MONGO:27017
+    // MONGO -> the name used on docker.compose file
+    mongoUrl: process.env.MONGO_URL || "mongodb://mongo:27017/clean-node-api",
     port: process.env.PORT || 5050,
     jwtSecret: process.env.JWT_SECRET || "5|sp{9DP5jj47<mN",
 };
